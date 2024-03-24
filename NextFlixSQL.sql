08af3a4c2642d1ad34bbb873a6369d364ad29c2b
gunn197 <124351574+gunn197@users.noreply.github.com>
2024-03-24 15:20:08 +0700

    Add files via upload

---
+++ NextFlixSQL.sql
@@ -0,0 +1,53 @@
+﻿-- Truy vấn 1: Đếm số lượng bộ phim và chương trình truyền hình trên Netflix
+SELECT 
+    type,
+    COUNT(*) AS count
+FROM 
+    netflix_data
+GROUP BY 
+    type;
+
+-- Truy vấn 2: Tìm 10 quốc gia có số lượng nhiều nhất các bộ phim và chương trình truyền hình trên Netflix
+SELECT 
+    country,
+    COUNT(*) AS count
+FROM 
+    netflix_data
+GROUP BY 
+    country
+ORDER BY 
+    count DESC
+LIMIT 10;
+
+-- Truy vấn 3: Tìm 10 đạo diễn phổ biến nhất trên Netflix
+SELECT 
+    director,
+    COUNT(*) AS count
+FROM 
+    netflix_data
+GROUP BY 
+    director
+ORDER BY 
+    count DESC
+LIMIT 10;
+
+-- Truy vấn 4: Tính trung bình độ dài của các bộ phim và chương trình truyền hình trên Netflix
+SELECT 
+    type,
+    AVG(duration) AS average_duration
+FROM 
+    netflix_data
+GROUP BY 
+    type;
+
+-- Truy vấn 5: Tính số lượng bộ phim và chương trình truyền hình được thêm vào Netflix theo từng năm
+SELECT 
+    release_year,
+    type,
+    COUNT(*) AS count
+FROM 
+    netflix_data
+GROUP BY 
+    release_year, type
+ORDER BY 
+    release_year;
